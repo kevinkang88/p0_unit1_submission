63d92c78a0c8e7d8ce2e711aa88e7f5201bcb723
kevinkang88 <kevinkang88@gmail.com>
2014-03-24 12:44:54 -0700

    add psudocode and initial solution to letter grade ex

--- 2_calculate_letter_grade/my_solution.rb
+++ 2_calculate_letter_grade/my_solution.rb
@@ -1,22 +1,29 @@
 # U1.W3: Calculate a letter grade!
 
-# Complete each step below according to the challenge directions and 
-# include it in this file. Also make sure everything that isn't code
-# is commented in the file.  
-
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# define a method named, get_grade, that takes parameter of array
+# find the average of those values 
+# if average is between 90,100 return string A
+# if average is between 80,90  return string B
+# if average is between 70,80  return string C
+# if average is between 60,70  return string D
+# everything else return string              F
 
 
 # 2. Initial Solution
-
-
-
+def get_grade(score_ary)
+  avg = (score_ary.inject {|memo,n| memo + n})/(score_ary.size)
+  case avg 
+    when (90..100) then "A"
+    when (80...90) then "B"
+    when (70...80) then "C"
+    when (60...70) then "D"
+    else                "F"
+  end
+end
 
 # 3. Refactored Solution
 
