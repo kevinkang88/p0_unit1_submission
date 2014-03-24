8f92c6c402744266b5b059225f529385b471015b
kevinkang88 <kevinkang88@gmail.com>
2014-03-23 22:57:29 -0700

    copy over final answer from stypi to calculate array total

--- 1_calculate_array_total/my_solution.rb
+++ 1_calculate_array_total/my_solution.rb
@@ -14,8 +14,25 @@
 
 
 # 2. Initial Solution
+# def total(array)
+#   array.inject(0) {|memo,n| memo + n}
+# end
+
+# def sentence_maker(array)
+#     array.join(" ").capitalize! + "."
+# end
 
 
+def total(num_array)
+  num_array.inject(0) {|memo,n| memo + n}
+end
+# define method called sentence_maker 
+# it will take one argument (an array)
+# it will join all elements and capitalize the first word and add a period at the end
+
+def sentence_maker(array)
+    "#{array.join(" ").capitalize!}."
+end
 
 
 # 3. Refactored Solution
