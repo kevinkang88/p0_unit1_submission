6006dea1ee072257a61485e9fe558393acd4f025
kevinkang88 <kevinkang88@gmail.com>
2014-03-24 12:49:41 -0700

    add reflection to calculate letter grade

--- 2_calculate_letter_grade/my_solution.rb
+++ 2_calculate_letter_grade/my_solution.rb
@@ -26,7 +26,17 @@ def get_grade(score_ary)
 end
 
 # 3. Refactored Solution
-
+# I do not see how I can improve this code. It is concise and passes all the tests.
+# I can see that case statement can be repetitive and may not follow DRY rule
+# but I do not know how to refactor the code	
 
 
 # 4. Reflection 
+# My strategy of getting an average and comparing them to corresponding ranges
+# worked. I used case statement instead of if/else statement from the start and
+# the code came out more clean. I had a question when the spec file had something
+# errors but after the correction, the process went smoothly. New skill that I
+# learned was chaining methods after closing curly brace which I did not know
+# it was possible until I experimented with them. I actually enjoyed this challenge
+# when there was an error in the spec file. I had to squeeze my brain to get an answer
+# and it was challenging. 
