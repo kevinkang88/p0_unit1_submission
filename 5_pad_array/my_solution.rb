6564f9d2e0e75e41ea9ddcf4d7ec043c91b48d70
kevinkang88 <kevinkang88@gmail.com>
2014-03-26 09:33:17 -0700

    add initial and refactored solution to pad an array exercise

--- 5_pad_array/my_solution.rb
+++ 5_pad_array/my_solution.rb
@@ -1,27 +1,72 @@
 # U1.W3: Pad an Array!
 
-# I worked on this challenge [by myself, with: ].
-
-# Complete each step below according to the challenge directions and 
-# include it in this file. Also make sure everything that isn't code
-# is commented in the file.  
-
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself, with: Jon Pabico].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
-
+#define a Array#pad method that accepts minimum size integer x,and an optional pad value y as parameters
+#if the array(ex.[1,2,3]) is less than x(ex.5) return a new array with original minimum length x(ex.[1,2,3,y,y]]
+#if the x is less than or equal to Array size than return original array
+#
+#define a Array#pad! method that accepts minimum size integer and an optional pad value as parameters
+#same as Array#pad but minipulate the original array
+#
+#
 
 # 2. Initial Solution
 
-
-
+class Array
+  def pad(min_size,opt_pad=nil)
+    new_array =  Array.new() + self
+    array_length = self.length
+    pad_size = min_size - array_length
+    if min_size <= array_length
+      return new_array
+    elsif min_size > array_length
+      pad_size.times do
+        new_array.push(opt_pad)
+      end
+      return new_array
+    end
+  end
+  
+  def pad!(min_size,opt_pad=nil)
+    extra = min_size - self.length
+    if min_size <= self.length
+      self 
+    elsif min_size > self.length
+      extra.times do
+        self.push(opt_pad)
+      end
+      self
+    end
+  end
+end
 
 # 3. Refactored Solution
 
+class Array
+  def pad(min_size,opt_pad=nil)
+    new_array = [] + self
+    array_length = self.length
+    pad_size = min_size - array_length
+    if min_size <= array_length
+      new_array
+    else
+      pad_size.times { new_array.push(opt_pad) }
+    end
+    new_array
+  end
 
+  def pad!(min_size,opt_pad=nil)
+    pad_size = min_size - self.length
+    if min_size <= self.length
+      self 
+    else
+      pad_size.times { self.push(opt_pad) }
+    end
+    self
+  end
+end
 
 # 4. Reflection 
