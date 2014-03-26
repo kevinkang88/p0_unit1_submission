939b2ddc619479e0bf502cbae5b4bcc234a1422c
kevinkang88 <kevinkang88@gmail.com>
2014-03-26 13:55:42 -0700

    add first person solution to RR pad an Array

--- 6_rr_pad_array/my_solution.rb
+++ 6_rr_pad_array/my_solution.rb
@@ -1,14 +1,42 @@
 # U1.W3: Review and Refactor: Pad an Array
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself]
 
 
 
 # 1. First Person's solution I liked
-#    What I learned from this solution
+# catherinev took different path compared to my solution in solving this
+# problem. They started writing code for Array#pad! since, it was faster 
+# to write destructive self than the opposite. I also thought Array#map 
+# was a clever way to assign self a new object id without altering its 
+# contents. Also learned how for..in iteration method can be useful to
+# adding paddings as well. To improve this code, they could eliminate 
+# explicit return keywords.    
 # Copy solution here:
+class Array 
+    def pad (number, object = nil)
+		new_array = self.map{|a| a}
+		if new_array.length >= number
+			return new_array
+		else
+			for i in new_array.length...number
+				new_array [i] = object
+			end
+			return new_array
+		end
+	end
 
-
+	def pad! (number, object = nil)
+		if self.length >= number
+			return self
+		else
+			for i in self.length...number
+				self [i] = object
+			end
+			return self
+		end
+	end
+end
 
 
 
