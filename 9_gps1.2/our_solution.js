7d2f9fd4f330c957a991f56d86a925bf94988b1d
kevinkang88 <kevinkang88@gmail.com>
2014-03-30 11:45:40 -0700

    update final solution for gps 1.2

--- 9_gps1.2/our_solution.js
+++ 9_gps1.2/our_solution.js
@@ -1,22 +1,52 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Hunter Chapman
+//  2. Kevin Kang
 
 
 // 1. "YOU SIGNED... WHO?!"
+var clientList = [];
 
+var willSmith = new Client(
+  "Will Smith",
+  48,
+  "Help me!");
+clientList.push(willSmith);
 
-
+var seanConnery = new Client(
+  "Sean Connery",
+  70,
+  "One ping only, please.");
+clientList.push(seanConnery);
 
 // 2. "Here they Come!"
 
+var adamSandler = new Client(
+  "Adam Sandler",
+  47,
+  "That's your home! Are you too god for your home?!");
+clientList.push(adamSandler);
 
+var kristenBell = new Client(
+  "Kristen Bell",
+  33,
+  "Do you wanna build a snowman?");
+clientList.push(kristenBell);
 
-
+var jimCarrey = new Client(
+  "Jim Carrey",
+  52,
+  "...So you're telling me there's a chance? YEAH!");
+clientList.push(jimCarrey);
 
 // 3. "TIME IS MONEY!"
+
+function Client(name, age, quote) {
+  this.name = name;
+  this.age = age;
+  this.quote = quote;
+}
 
 //YOUR CODE HERE!
 
@@ -31,7 +61,14 @@ shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to wh
 
 // 4. "SHOW 'EM OFF!"
 
+Client.prototype.show = function() {
+  console.log(this.name + " is " + this.age + " years old. " + "Famous Line: " + this.quote);
+}
+
 
 
 
 // ** BONUS **
+Client.prototype.ad = function() {
+  alert(this.name + " says: Represent'en from the agency that is super awesome!");  
+}
